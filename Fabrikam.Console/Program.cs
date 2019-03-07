47bd09285d6e432c9c776641849492afb0c8864f
Immo Landwerth <immol@microsoft.com>
2019-03-07 09:41:42 -0600

    Initial commit

---
+++ Fabrikam.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using Fabrikam.Logging;
+using System;
+
+namespace Fabrikam.Console
+{
+    class Program
+    {
+        static void Main()
+        {
+            var logEntry = new
+            {
+                StartTime = DateTime.Now,
+                Purpose = "Demo",
+                State = "Started"
+            };
+            MyLogger.Instance.WriteLine(logEntry);
+        }
+    }
+}
